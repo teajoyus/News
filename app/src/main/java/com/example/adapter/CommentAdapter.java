b3e5ae4d71f7eb55fb885cda8d911b6c167f0402
teajoyus <625321521@qq.com>
2016-10-15 13:18:44 +0800

    修改了一个测试

--- app/src/main/java/com/example/adapter/CommentAdapter.java
+++ app/src/main/java/com/example/adapter/CommentAdapter.java
@@ -35,6 +35,7 @@ import java.util.List;
 /**
  * Created by Administrator on 2016/5/26 0026.
  */
+
 public class CommentAdapter extends BaseAdapter {
     private Context context;
     private List<CommentItem> list;
